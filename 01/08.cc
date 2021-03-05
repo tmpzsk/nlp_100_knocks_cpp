02c7f40ba1f3f0fcee450bc97808cdac9bbce8b2
hikimochi <rhikimochi@gmail.com>
2021-03-05 22:10:44 +0900

    Add 08.

---
+++ 01/08.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+    vector<int> key;
+    cout << "raw: " << argv[1] << endl;
+    // 暗号化
+    for(int i = 0; i < sizeof(argv[1]); i++) {
+        if (islower(argv[1][i])) {
+            key.push_back(i);
+            argv[1][i] = 219 - +argv[1][i]; 
+        }
+        
+    }
+    cout << "暗号化: " << argv[1] << endl;
+    // 復号化
+    for (int i : key) {
+        argv[1][i] = 219 - +argv[1][i]; 
+    }
+    cout << "復号化: " << argv[1] << endl;
+}
