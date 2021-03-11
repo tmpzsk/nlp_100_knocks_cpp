1dc6015cfe8e1d1cfb1cc190a1ecaffb38aaf82a
hikimochi <rhikimochi@gmail.com>
2021-03-11 23:15:17 +0900

    Add 15.

---
+++ hikimochi/02/15.cc
@@ -0,0 +1,39 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <vector>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    ifstream ifs(argv[1]);
+    int n = atoi(argv[2]);
+    string s;
+    vector<string> vec;
+
+
+    if (ifs.fail()) {
+        cerr << "Failed to open file. " << argv[1] <<endl;
+        return -1;
+    }
+    if ( n < 0 ) {
+        cerr << "Please use positive number." << endl;
+    }
+
+    int cnt = 0;
+    // 行数のカウント
+    while(getline(ifs, s)) { 
+        vec.push_back(s);
+        cnt++;
+    }
+    // 出力を始める行番号（ファイルの行数を超えるときはすべて出力）
+    int start_line;
+    if ( (cnt - n) < 0 ) start_line = 0;
+    else start_line = cnt - n;
+    // 出力
+    for (int i = 0; i < vec.size(); ++i) {
+        if (i >= start_line) {
+            cout << vec[i] << endl;
+        }
+    }
+} 
