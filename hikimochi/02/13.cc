02d039b2580a1683364d44b817f65a3b63550b53
hikimochi <rhikimochi@gmail.com>
2021-03-11 21:44:32 +0900

    Add 13.

---
+++ hikimochi/02/13.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    ifstream ifs1("./13/col1.txt");
+    ifstream ifs2("./13/col2.txt");
+    ofstream outputfile(argv[1]);
+    string s1, s2;
+
+    while ( ( getline(ifs1, s1) && getline(ifs2, s2) ) || getline(ifs2, s2) ) {
+        outputfile << s1 << "\t" << s2 << endl;
+    }
+    outputfile.close();
+}
