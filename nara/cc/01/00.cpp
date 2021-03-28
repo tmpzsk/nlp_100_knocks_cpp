775b6dc83187ff7454daadfc26aec2780c238e49
s.n <s.n.develop.6@gmail.com>
2021-03-28 14:56:03 +0900

    no message

--- nara/cc/01/00.cpp
+++ nara/cc/01/00.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-string reverse_s(string s) {
+string reverse_s(const string s) {
     int n = s.length();
     string r_s = "";
     for (int i = 0; i < n; ++i) {
@@ -11,7 +11,7 @@ string reverse_s(string s) {
 }
 
 int main() {
-    string s = "stressed";
+    const string s = "stressed";
     string rev_s = reverse_s(s);
     cout << rev_s << endl;
 }
