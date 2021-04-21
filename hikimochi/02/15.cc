977a64ee9d6009ea75edcb1093936494c5d383fa
hikimochi <rhikimochi@gmail.com>
2021-04-22 00:03:51 +0900

    Fix for review.

--- hikimochi/02/15.cc
+++ hikimochi/02/15.cc
@@ -3,35 +3,32 @@
 #include <string>
 #include <stdlib.h>
 #include <vector>
-using namespace std;
 
 int main(int argc, char *argv[]) {
-    ifstream ifs(argv[1]);
+    // filename
+    std::ifstream ifs(argv[1]);
+    // line number
     int n = atoi(argv[2]);
-    string s;
-    vector<string> vec;
-
-
     if (ifs.fail()) {
-        cerr << "Failed to open file. " << argv[1] <<endl;
+        std::cerr << "Failed to open file. " << argv[1] << std::endl;
         return -1;
     }
     if ( n < 0 ) {
-        cerr << "Please use positive number." << endl;
+        std::cerr << "Please use positive number." << std::endl;
     }
 
+    std::string s;
+    std::vector<std::string> vec;
     // 行数のカウント
     while(getline(ifs, s)) { 
         vec.push_back(s);
     }
     // 出力を始める行番号（ファイルの行数を超えるときはすべて出力）
-    int start_line;
-    if ( (vec.size() - n) < 0 ) start_line = 0;
-    else start_line = vec.size() - n;
+    const int start_line = vec.size() - n < 0 ? 0 : vec.size() - n;
     // 出力
     for (int i = 0; i < vec.size(); ++i) {
         if (i >= start_line) {
-            cout << vec[i] << endl;
+            std::cout << vec[i] << std::endl;
         }
     }
 } 
